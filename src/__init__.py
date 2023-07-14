0d3b2925bd68974b3cac3acecbd2fd4b88c19311
IDDT <IDDT@users.noreply.github.com>
2023-07-14 23:28:52 +0700

    Fix order.

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from flask import Flask, request, jsonify
 from .grammar import gen_grammar
 from .llamacpp import infer
@@ -12,10 +13,16 @@ def index():
     body = request.get_json()
     prompt = str(body.get('prompt', ''))
     schema = body.get('schema', {'type': 'object', 'properties': {}})
-    key_order = [str(x) for x in body.get('order', [])]
-    print(prompt, schema)
-    grammar = gen_grammar(schema)
+    order = [str(x) for x in body.get('order', [])]
+
+    # Generate grammar.
+    grammar = gen_grammar(schema, order)
+
+    # Run inference.
     s = infer(grammar, prompt)
-    print(s)
+    logging.info(s)
+
+    # Parse output as json.
     out = json.loads(s.strip().removeprefix(prompt))
+
     return jsonify(out)
--- src/config.py
+++ src/config.py
@@ -1,7 +1,19 @@
 import os
+import sys
+import logging
 
 
+# Logging.
+logging.basicConfig(
+    format='%(asctime)s,%(msecs)03d %(levelname)s %(message)s',
+    datefmt='%Y-%m-%dT%H:%M:%S',
+    level=logging.INFO,
+    stream=sys.stdout,
+    force=True
+)
+
+
+# Paths.
 TEMP_DIR = 'temp'
-
 BINARY_PATH = os.path.join(TEMP_DIR, 'binary')
 MODEL_PATH = os.path.join(TEMP_DIR, 'model.bin')
